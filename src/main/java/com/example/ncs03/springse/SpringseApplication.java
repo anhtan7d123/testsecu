0eabf49857c0a3ec5203211d66e4b447d785e7ff
anhtan7d123 <tanltn123456@gmail.com>
2022-11-25 16:29:56 +0700

    add project

---
+++ src/main/java/com/example/ncs03/springse/SpringseApplication.java
@@ -0,0 +1,13 @@
+package com.example.ncs03.springse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringseApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/ncs03/springse/SpringseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ncs03.springse;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
